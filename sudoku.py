60ee6ee44c6e1ff6df35efbee5cee4dc0f132b8a
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-05-25 05:46:09 +0700

    wrote mainloop

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,20 @@
+import pygame as pg
 
+pg.init()
+
+screen = pg.display.set_mode((500, 550))
+pg.display.set_caption('Sudoku')
+
+clock = pg.time.Clock()
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+
+
+    screen.fill((192,96,69))
+
+    pg.display.update()
+    clock.tick(60)
