317b351c9d8405b879462b7e5200a3ceb02afb8f
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-05-25 11:23:10 +0700

    cells done

--- sudoku.py
+++ sudoku.py
@@ -13,29 +13,29 @@ x, y = 0, 0
 for i in range(9):
     row = []
     for j in range(9):
-        if i == 0:
-            y = 8
-            if j == 0:
-                x = 8
-            else:
-                x = cells[i][j-1][0] + 53
-                if j == 3 or j == 6:
-                    x += 5
-        elif j == 0:
-            x = 8
-            y = cells[i-1][j][1] + 53
-            if i == 3 or i == 6:
-                y += 5
-        else:
-            x = cells[i - 1][j][0] + 53
-            y = cells[i][j - 1][1] + 53
-            if i == 3 or i == 6:
-                x += 5
-            if j == 3 or j == 6:
-                y += 5
-        cell = (x, y)
+        cell = [0, 0]
         row.append(cell)
     cells.append(row)
+
+
+for i in range(9):
+    for j in range(9):
+        if i == 0 and j == 0:
+            cells[i][j][0] = cells[i][j][1] = 8
+        else:
+            if i == 0:
+                cells[i][j][1] = 8
+                cells[i][j][0] = cells[i][j - 1][0] + 53
+            elif j == 0:
+                cells[i][j][0] = 8
+                cells[i][j][1] = cells[i - 1][j][1] + 53
+            else:
+                cells[i][j][0] = cells[i][j - 1][0] + 53
+                cells[i][j][1] = cells[i - 1][j][1] + 53
+            if i == 3 or i == 6:
+                cells[i][j][1] += 8
+            if j == 3 or j == 6:
+                cells[i][j][0] += 8
 
 print(cells)
 
@@ -57,8 +57,9 @@ while True:
         for j in range(3):
             pg.draw.rect(screen, 'white', (i*(box_size+5)+5, j*(box_size+5)+5, box_size, box_size), 0, 5)
 
-    # for (x, y) in cells:
-    #     pg.draw.rect(screen, 'light yellow', (x, y, 50, 50), 0, 2)
+    for row in cells:
+        for x,y in row:
+            pg.draw.rect(screen, 'green', (x, y, 50, 50), 0, 2)
 
     pg.display.update()
     clock.tick(60)
