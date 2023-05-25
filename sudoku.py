0c76c20074488b0f8710284a56b7ad8fbb040923
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-05-25 09:42:55 +0700

    draw big squares

--- sudoku.py
+++ sudoku.py
@@ -2,10 +2,11 @@ import pygame as pg
 
 pg.init()
 
-screen = pg.display.set_mode((500, 550))
+screen = pg.display.set_mode((506, 550))
 pg.display.set_caption('Sudoku')
-
 clock = pg.time.Clock()
+
+box_size = 162
 
 while True:
     for event in pg.event.get():
@@ -15,6 +16,9 @@ while True:
 
 
     screen.fill((192,96,69))
+    for i in range(3):
+        for j in range(3):
+            pg.draw.rect(screen, 'white', (i*(box_size+5)+5, j*(box_size+5)+5, box_size, box_size), 0, 5)
 
     pg.display.update()
     clock.tick(60)
